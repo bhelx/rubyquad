a56662257e71d3b4533ac9584e837d95640ffd73
Benjamin Eckel <benjamin.eckel@gmail.com>
2011-05-26 13:21:16 -0500

    minor updates, need to reorganize main code and check for memory leaks

--- rubyquad.c
+++ rubyquad.c
@@ -1,7 +1,7 @@
-#include "ruby.h"
+#include <ruby.h>
 #include "quadtree.h"
 
-VALUE cQuadTree;
+static VALUE cQuadTree;
 
 static VALUE quad_tree_init(VALUE self, VALUE north, VALUE south, VALUE east, VALUE west) {
 
@@ -20,14 +20,27 @@ static VALUE quad_tree_init(VALUE self, VALUE north, VALUE south, VALUE east, VA
   return self;
 }
 
+static VALUE quad_tree_add_point(VALUE self, VALUE x, VALUE y, VALUE z) {
+ 
+  QuadTree *tree;
+  Data_Get_Struct(self, QuadTree, tree);
+
+  Point *point = createPoint(NUM2DBL(x), NUM2DBL(y), NUM2DBL(z));
+  insert(tree, point);
+
+  return self;
+}
+
 VALUE quad_tree_new(VALUE class, VALUE north, VALUE south, VALUE east, VALUE west) {
+
+  QuadTree *tree = createQuadTree(NUM2DBL(north), NUM2DBL(south), NUM2DBL(east), NUM2DBL(west), NULL);
+
   VALUE argv[4];
   argv[0] = north;
   argv[1] = south;
   argv[2] = east;
   argv[3] = west;
 
-  QuadTree *tree = createQuadTree(NUM2DBL(north), NUM2DBL(south), NUM2DBL(east), NUM2DBL(west), NULL);
 
   VALUE tdata = Data_Wrap_Struct(class, 0, -1, tree);
   rb_obj_call_init(tdata, 4, argv);
@@ -39,6 +52,7 @@ void Init_rubyquad() {
   cQuadTree = rb_define_class("QuadTree", rb_cObject);
   rb_define_singleton_method(cQuadTree, "new", quad_tree_new, 4);
   rb_define_method(cQuadTree, "initialize", quad_tree_init, 4);
+  rb_define_method(cQuadTree, "insert", quad_tree_add_point, 3);
 
 }
 
